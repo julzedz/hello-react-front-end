fa7c3bd67020bd5efc86da1bc4f7671b6901b5c5
julzedz <edoziechukwuma@gmail.com>
2023-08-17 15:35:21 +0100

    Modify main and app files

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import store from './Store.jsx'
+import store from './redux/store'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
